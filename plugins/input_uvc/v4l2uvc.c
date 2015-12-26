9a037b782f3ed2851d0707f9e152955955127423
Scott Weston <scottw@gmail.com>
2015-12-26 14:17:31 +1100

    fix for latest RPI firmware

--- plugins/input_uvc/v4l2uvc.c
+++ plugins/input_uvc/v4l2uvc.c
@@ -418,6 +418,7 @@ int uvcGrab(struct vdIn *vd)
 {
 #define HEADERFRAME1 0xaf
     int ret;
+    int32_t bytesused;
 
     if(vd->streamingState == STREAMING_OFF) {
         if(video_enable(vd))
@@ -432,6 +433,7 @@ int uvcGrab(struct vdIn *vd)
         perror("Unable to dequeue buffer");
         goto err;
     }
+    bytesused = vd->buf.bytesused;
 
     switch(vd->formatIn) {
     case V4L2_PIX_FMT_MJPEG:
@@ -468,6 +470,7 @@ int uvcGrab(struct vdIn *vd)
     }
 
     ret = xioctl(vd->fd, VIDIOC_QBUF, &vd->buf);
+    vd->buf.bytesused = bytesused;
     if(ret < 0) {
         perror("Unable to requeue buffer");
         goto err;
